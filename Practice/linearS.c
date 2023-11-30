5c218bea28538bf2282c095c575e71c80243ab06
aayushker <singhaayushker@gmail.com>
2023-11-30 14:58:53 +0530

    Practiced some codes

--- Practice/linearS.c
+++ Practice/linearS.c
@@ -1,32 +1,30 @@
 #include<stdio.h>
 
-int binaryS(int a[], int n, int ele){
-    for (int i = 0; i<n; i++){
-        if (a[i]==ele){
-            return i+1;
-        }
+void  merge(int a[], int l, int m, int r){
+    int i=l, j = m+1, k=l;
+    temp = 
+}
+
+
+void display(int arr[], int size){
+    printf("The array is:\n" );
+    for (int i=0; i<size; i++){
+        printf("%d\n",arr[i]);
     }
-    return -1;
 }
 
 int main(){
-    int n,ele;
-    printf("Enter the total number of elements you want to enter");
-    scanf("%d",&n);
-    int a[n];
-    for (int i=0; i<n; i++){
-        scanf("%d",&a[i]);
-    }
-    int pos;
-    printf("Enter the element you want to find");
-    scanf("%d",&ele);
-    pos = binaryS(a, n, ele);
-    if (pos==-1){
-        printf("Element not found\n");
-    }
-    else{
-        printf("Element found at location %d",pos);
-    }
-    return 0;
+    int size;
+    printf("Enter how many elements you want to enter: ");
+    scanf("%d",&size);
 
+    int arr[size];
+    printf("Enter the elements:\n");
+    for (int i=0; i<size; i++){
+        scanf("%d",&arr[i]);
+    }
+    insertion(arr, size);
+    display(arr, size);
+
+    return 0;
 }
